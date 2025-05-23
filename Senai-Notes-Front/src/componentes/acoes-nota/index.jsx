cdf8c051c0b0b8fa3637a84f2402d47cb70f44b3
Yan Victor Da Silva Lima <yan.lima@scseduca.com.br>
2025-05-22 21:32:14 -0300

    funcionalidades do botao de arquivar e deletar

--- Senai-Notes-Front/src/componentes/acoes-nota/index.jsx
+++ Senai-Notes-Front/src/componentes/acoes-nota/index.jsx
@@ -7,13 +7,88 @@ import Delete from '../../assets/ImgNotas/Delete.svg'
 
 
 
-function AcoesNota() {
+function AcoesNota({ notaSelecionada, aoFecharANota }) {
+    const baseUrl = "http://localhost:3000/notas";
+
+    const onArchiveNote = async () => {
+
+        const response = await fetch(`${baseUrl}/${notaSelecionada.id}`, {
+            method: "PATCH",
+            headers: { 
+                "Content-Type": "application/json",
+            }, 
+            body: JSON.stringify({ archived: true }),
+        });
+
+        if (!response.ok) {
+
+            alert("Erro ao arquivar a nota");
+
+        } else {
+
+            alert(`Nota "${notaSelecionada.title}" arquivada!`)
+        }
+
+        aoFecharANota();
+
+    }
+
+    const onUnarchiveNote = async () => {
+
+        const response = await fetch(`${baseUrl}/${notaSelecionada.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ archived: false }),
+        });
+
+        if (!response.ok) {
+
+            alert (`Nota "${notaSelecionada.title}" desarquivada!`);
+
+        }
+
+        aoFecharANota();
+
+    };
+
+    const onDeleteNote = async () => {
+
+        const response = await fetch(`${baseUrl}/${notaSelecionada.id}`,{
+            method: "DELETE",
+        });
+
+        if (!response.ok) {
+
+            alert("Erro ao deletar a nota");
+        
+        } else {
+
+            alert(`Nota "${notaSelecionada.title}" deletada!`)
+
+        }
+
+        aoFecharANota();
+    };
+
     return (
         <>
             <div className="acoes-nota">
                 <div className="caixa-botoes">
-                    <button className="botao"><img src={Archive} />Archive Note</button>
-                    <button className="botao"><img src={Delete} />Delete Note</button>
+                    {notaSelecionada.archived && (
+                    <button onClick={onUnarchiveNote}
+                    className="botao"><img src={Archive} />Unarchive Note</button>
+                    )}
+
+                    {!notaSelecionada.archived && (
+                    <button onClick={onArchiveNote}
+                    className="botao"><img src={Archive} />Archive Note</button>
+                    )}
+
+                        
+                    <button onClick={onDeleteNote}
+                     className="botao"><img src={Delete} />Delete Note</button>
                 </div>
             </div>
 
